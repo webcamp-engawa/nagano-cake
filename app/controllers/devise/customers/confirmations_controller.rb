fd0b1b945d06a60b24cfd621622c90b64cd5068b
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-01-09 19:45:16 +0900

    deviseのview不具合を修正

--- app/controllers/devise/customers/confirmations_controller.rb
+++ app/controllers/devise/customers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Devise::Customer::ConfirmationsController < Devise::ConfirmationsController
+class Devise::Customers::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
--- app/controllers/devise/customers/omniauth_callbacks_controller.rb
+++ app/controllers/devise/customers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Devise::Customer::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Devise::Customers::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
