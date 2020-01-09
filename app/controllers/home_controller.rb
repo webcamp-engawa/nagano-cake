c1212e377a14dbb4d257dbcf0bce8151ae45e5f5
tanakashun <michael021212@gmail.com>
2020-01-09 21:29:37 +0900

    [UPDATE]homeとitemのConとView

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def top
-
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.all
+    else
+      @genres = Genre.all
+      @items = Item.all
+    end
   end
 
   def about
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    if params[:genre_id]
+      @items = Item.where(genre_id: params[:genre_id])
+      @genres = Genre.all
+    else
+      @genres = Genre.all
+      @items = Item.all
+    end
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
