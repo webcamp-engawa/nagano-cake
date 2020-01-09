0748f5152495da445b4913972bc249a62d9b11ad
natsuki525 <eteespoir@icloud.com>
2020-01-09 08:18:20 +0000

    [Add]admin/items controller

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,20 @@
+class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
