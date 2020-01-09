13dd3c42649d50038f8e73ed8605b7ce07141823
natsuki525 <eteespoir@icloud.com>
2020-01-09 04:30:24 +0000

    [Add]Validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :cart_items
   belongs_to :genre
+  validates :genre_id, presence: true, numericality: :only_integer
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :image_id, presence: true
+  validates :price, presence: true, numericality: {only_integer: true}
+  validates :is_sold, inclusion: { in: [true, false] }
+  attachment :image
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
+
+  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は「○○○-○○○○」で入力して下さい。' }
+  validates :address_name, presence: true
+  validates :payment,  presence: true
 end
