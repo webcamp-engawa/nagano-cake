8b33b2fc11a8a8be21a0c73cc2ed580c7acd02ae
tanakashun <michael021212@gmail.com>
2020-01-10 15:46:42 +0900

    [UPDATE]cart_itemsのConとバリデーション

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,15 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_item = CartItem.all
+    @cart_item.customer_id = current_customer.id
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = params[:id]
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
@@ -14,4 +21,8 @@ class CartItemsController < ApplicationController
   def empty_cart
   end
 
+    private
+    def cart_item_params
+      params.require(:cart_item).permit(:item_id, :quantity)
+    end
 end
