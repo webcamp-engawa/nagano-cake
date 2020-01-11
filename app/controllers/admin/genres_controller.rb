3b0b64a48e9d1c5ca885482dc20fd40429a7300b
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-11 11:51:36 +0900

    [create]genre_edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,7 @@
 class Admin::GenresController < ApplicationController
 
 	def index
-		@genles = Genre.all
+		@genres = Genre.all
 	end
 
 	def create
@@ -9,10 +9,21 @@ class Admin::GenresController < ApplicationController
 	end
 
 	def edit
-
+		@genre = Genre.find(params[:id])
 	end
 
 	def update
-
+		@genre = Genre.find(params[:id])
+		if @genre.update(genre_params)
+		redirect_to admin_genres_path
+		else
+		render 'edit'
+		end
 	end
+
+	private
+	def genre_params
+		params.require(:genre).permit(:name,:is_active)
+	end
+
 end
