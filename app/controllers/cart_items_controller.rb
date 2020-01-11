ac32347a215eb5ba61befd957ca13a414226b5a3
tanakashun <michael021212@gmail.com>
2020-01-11 15:35:38 +0900

    [Update]「カートに入れる」機能

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,15 +1,19 @@
 class CartItemsController < ApplicationController
   def index
-    @cart_item = CartItem.all
-    @cart_item.customer_id = current_customer.id
+    @cart_item = CartItem.where(customer_id: current_customer.id)
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.item_id = params[:id]
-    @cart_item.save
-    redirect_to cart_items_path
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      session[:cart_item] = @cart_item.attributes.slice(*cart_item_params.keys)
+      @genres = Genre.all
+      @item = Item.find_by(id:@cart_item.item_id)
+      redirect_to item_path(@item.id), flash: {alert: '※個数を選択して下さい'}
+    end
   end
 
   def update
