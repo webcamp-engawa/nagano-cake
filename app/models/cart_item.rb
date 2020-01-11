4c5845c2340d1fedf143176c67933527c25263f3
tanakashun <michael021212@gmail.com>
2020-01-11 15:02:54 +0900

    [Fix]バリデーションメッセージは使えなくなったので、記述を削除

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,5 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  validates :quantity, presence: { message: '個数を洗濯して下さい' }
+  validates :quantity, presence: true
 end
