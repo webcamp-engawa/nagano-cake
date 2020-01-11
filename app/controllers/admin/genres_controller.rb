ac78f68a6958bdb96761e1b10092ca2226b48657
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-11 12:07:44 +0900

    [create]genre_create

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,11 +1,18 @@
 class Admin::GenresController < ApplicationController
 
 	def index
+		@genre = Genre.new
 		@genres = Genre.all
 	end
 
 	def create
-
+		@genre = Genre.new(genre_params)
+		if @genre.save
+		redirect_to admin_genres_path
+		else
+		@genres = Genre.all
+		render 'index'
+		end
 	end
 
 	def edit
