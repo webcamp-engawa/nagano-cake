99123fdd3b12fd62202184960aa3219c41cae4ac
natsuki525 <eteespoir@icloud.com>
2020-01-12 07:04:37 +0000

    [fix]items画面修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,7 +14,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
-    redirect_to admin_item_path(@item.id)
+    redirect_to admin_item_path(@item)
   end
 
   def new
@@ -23,9 +23,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.admin_id = current_admin.admin_id
-    @item.save
-    redirect_to admin_item_path(@item.id)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
   end
 
   private
