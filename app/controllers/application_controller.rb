81bfb357fff5303cbaafe019d415095fdcd8ada9
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-12 19:52:04 +0900

    [fix]ユーザごとのヘッダー表示の分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,22 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
 		case resource
 			when Admin
-			admins_root_path
+			admin_root_path
 			when Customer
 			root_path
 		end
 	end
+
+
+def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
+
 
 	protected
 		def configure_permitted_parameters
