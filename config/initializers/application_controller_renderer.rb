a8a5903a545edacf042ea14d5ea72d37eb22a2b8
tanakashun <michael021212@gmail.com>
2020-01-12 18:15:55 +0900

    [add]田仲PCで出るRuntimeError at / Refile.secret_key was not set.の修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '431e7b9b618038e10b5104ca35c12f50a8116b622c595117848af2e33bd3e21c7e22cd865cbf5ddb60ef94b5cd62fbfa4774019e225dbff798b4e05c617cabba'
