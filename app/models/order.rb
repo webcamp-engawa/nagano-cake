33f9fd2248f3740f35144ecab7e9175194d9ea35
natsuki525 <eteespoir@icloud.com>
2020-01-12 14:48:10 +0000

    [fix]注文一覧画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,5 @@ class Order < ApplicationRecord
   validates :address_name, presence: true
   validates :payment,  presence: true
   enum payment: {クレジットカード:1, 銀行振込:2}
+  enum order_status: {入金待ち:1, 入金確認:2, 制作中:3, 発送準備中:4, 発送済み:5}
 end
