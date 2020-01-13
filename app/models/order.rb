eb10f8d491d5141bf8ea7cdd143006c1f81f5b56
tanakashun <michael021212@gmail.com>
2020-01-13 16:32:11 +0900

    [add]orderテーブルに隠しconfirming

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,17 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
 
-  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は「○○○-○○○○」で入力して下さい。' }
-  validates :address_name, presence: true
   validates :payment,  presence: true
-  enum payment: {クレジットカード:1, 銀行振込:2}
+  enum payment: { クレジットカード: 1, 銀行振込: 2 }
+  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は「○○○-○○○○」で入力して下さい。' }
+  validates :address, presence: true
+  validates :address_name, presence: true
+
+  validates_acceptance_of :confirming
+  after_validation :check_confirming
+
+  def check_confirming
+    errors.delete(:confirming)
+    self.confirming = errors.empty? ? '1' : ''
+  end
 end
