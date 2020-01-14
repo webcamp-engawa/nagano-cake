4be2973b24aaa9859cc6a6a4816520b7b3348a31
natsuki525 <eteespoir@icloud.com>
2020-01-14 10:24:03 +0000

    [fix] admin/order&item/view修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ApplicationRecord
   validates :address, presence: true
   validates :address_name, presence: true
 
-  enum order_status: {入金待ち:1, 入金確認:2, 制作中:3, 発送準備中:4, 発送済み:5}
+  enum order_status: {入金待ち:1, 入金確認:2, 製作中:3, 発送準備中:4, 発送済み:5}
 
   validates_acceptance_of :confirming
   after_validation :check_confirming
