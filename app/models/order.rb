5bb2c821508c03e9282469d3de95b454965d32bf
tanakashun <michael021212@gmail.com>
2020-01-15 11:54:38 +0900

    [fix]注文のお届け先情報の保持

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
   validates :address_name, presence: { message: 'が入力されていません。'}
 
   enum order_status: {入金待ち:1, 入金確認:2, 製作中:3, 発送準備中:4, 発送済み:5}
-
+  attr_accessor :shipping
   validates_acceptance_of :confirming
   after_validation :check_confirming
 
