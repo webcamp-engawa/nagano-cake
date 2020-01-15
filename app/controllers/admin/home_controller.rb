35bd9deaf0845fbbf350cc7c02d76a486d7e2bd8
natsuki525 <eteespoir@icloud.com>
2020-01-15 09:37:06 +0000

    [fix] 認証機能追加

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,8 @@
 class Admin::HomeController < ApplicationController
+
+before_action :authenticate_admin!
+
 	def top
-		#@order = Order.where(created_at: 1.day.ago.all_day
 			@order = Order.where("created_at >= ?", Time.zone.now.beginning_of_day)
 
 	end
