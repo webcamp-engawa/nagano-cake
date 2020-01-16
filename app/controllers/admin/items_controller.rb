954911303c1b144bd38a573b068999ce7a196121
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-16 19:19:54 +0900

    [fix]商品登録後のページ遷移

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -29,7 +29,7 @@ before_action :authenticate_admin!
     @item = Item.new(item_params)
     @genres = Genre.all
     if @item.save
-      redirect_to admin_item_path(@item)
+      redirect_to admin_items_path
     else
       render :new
     end
