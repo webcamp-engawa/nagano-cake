4cdec3374385397506a9a5488619423c1a17e640
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-18 19:53:47 +0900

    [create]トップページのレイアウト調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,22 +2,22 @@ class ItemsController < ApplicationController
   def index
     if params[:genre_id]
       @item = Item.where(genre_id: params[:genre_id])
-      @items = @item.where(is_sold: true)
-      @search_items = @items.where(is_sold: true)
-      @genres = Genre.page(params[:page])
+      @items = @item.where(is_sold: true).page(params[:page]).reverse_order.per(9)
+      @search_items = @items.where(is_sold: true).page(params[:page]).reverse_order.per(9)
+      @genres = Genre.page(params[:page]).per(9)
       @genres = @genres.where(is_active: true)
       @genre = Genre.find(params[:genre_id])
     elsif params[:q][:name_cont] == ""
       redirect_to root_path
     else
-      @genres = Genre.page(params[:pate])
+      @genres = Genre.page(params[:pate]).per(9)
       @genres = @genres.where(is_active: true)
 
     end
   end
 
   def show
-    @genres = Genre.all
+    @genres = Genre.where(is_active: true)
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
