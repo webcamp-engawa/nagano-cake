24c26df30581fc631bdbd0763e3da4d05370f28f
MacUser <macuser@MacUsernoMacBook-Pro.local>
2020-01-21 16:13:48 +0900

    [create]admim_home,customer_orderレイアウト調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders.page(params[:page]).reverse_order.per(9)
   end
 
   def show
